312e1f6efe5b062b015cdf81975e99e33e1ae583
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-09-16 22:54:12 +0530

    Create 160. Intersection of Two Linked Lists.cpp

---
+++ 160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+        unordered_set<ListNode*> st;
+        while(head1 != NULL) {
+             st.insert(head1);
+            head1 = head1->next;
+        }
+        while(head2 != NULL) {
+            if(st.find(head2) != st.end()) 
+                return head2;
+            head2 = head2->next;
+        }
+        return NULL;
+    }
+};
